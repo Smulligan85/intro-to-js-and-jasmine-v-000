62fec00b66094288f5144b8141800a9e48244612
Smulligan85 <sean.mulligan85@gmail.com>
2016-04-20 20:05:47 -0700

    Review

--- code.js
+++ code.js
@@ -1,1 +1,17 @@
 'use strict';
+
+function favIceCream(flavor) {
+  return "I love " + flavor;
+}
+
+function shouting(phrase) {
+  return phrase.toUpperCase();
+}
+
+function roundDown(num) {
+  return num.floor();
+}
+
+function theTruth {
+  return true;
+}
